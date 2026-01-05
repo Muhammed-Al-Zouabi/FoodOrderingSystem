483113535137b14bf991c0acd4c910b18444647f
malzb <malzb@DESKTOP-O3OUFIR.local>
2026-01-06 02:18:23 +0300

    Formatted Restaurant class code

--- src/com/arel/oop/Restaurant.java
+++ src/com/arel/oop/Restaurant.java
@@ -5,33 +5,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Restaurant {
-    private String name;
-    private List<Orderable> menu;
+	private String name;
+	private List<Orderable> menu;
 
-    public Restaurant(String name) {
-        this.name = name;
-        this.menu = new ArrayList<>();
-    }
+	public Restaurant(String name) {
+		this.name = name;
+		this.menu = new ArrayList<>();
+	}
 
-    // Method to add food to the menu
-    public void addToMenu(Orderable item) {
-        menu.add(item);
-    }
+	// Method to add food to the menu
+	public void addToMenu(Orderable item) {
+		menu.add(item);
+	}
 
-    // Method to show the menu to the customer
-    public void displayMenu() {
-        System.out.println("\n=== " + name.toUpperCase() + " MENU ===");
-        for (int i = 0; i < menu.size(); i++) {
-            Orderable item = menu.get(i);
-            System.out.println((i + 1) + ". " + item.getName() + " - " + item.getPrice() + " TL");
-        }
-    }
+	// Method to show the menu to the customer
+	public void displayMenu() {
+		System.out.println("\n=== " + name.toUpperCase() + " MENU ===");
+		for (int i = 0; i < menu.size(); i++) {
+			Orderable item = menu.get(i);
+			System.out.println((i + 1) + ". " + item.getName() + " - " + item.getPrice() + " TL");
+		}
+	}
 
-    // Method to pick an item from the list
-    public Orderable getItem(int index) {
-        if (index >= 0 && index < menu.size()) {
-            return menu.get(index);
-        }
-        return null;
-    }
+	// Method to pick an item from the list
+	public Orderable getItem(int index) {
+		if (index >= 0 && index < menu.size()) {
+			return menu.get(index);
+		}
+		return null;
+	}
 }
