718a27a6a2be37b7c0e90ea42a9c84e3051e9402
malzb <malzb@DESKTOP-O3OUFIR.local>
2026-01-06 02:10:44 +0300

    Added getter for phone number

---
+++ src/com/arel/oop/Customer.java
@@ -0,0 +1,17 @@
+package com.arel.oop;
+
+public class Customer extends User {
+    private String address;
+    private String phone;
+
+    public Customer(String id, String name, String email, String address, String phone) {
+        super(id, name, email); // This fixes the inheritance link to User
+        this.address = address;
+        this.phone = phone;
+    }
+
+    @Override
+    public void displayRole() {
+        System.out.println("Role: Customer");
+    }public String getPhone() { return phone; }
+}
