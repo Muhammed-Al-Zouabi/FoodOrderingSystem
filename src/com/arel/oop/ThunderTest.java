bb3db9a48b40e07e991eb8c876de90dffd8fb914
malzb <malzb@DESKTOP-O3OUFIR.local>
2026-01-06 13:50:29 +0300

    Add second JUnit test

--- src/com/arel/oop/ThunderTest.java
+++ src/com/arel/oop/ThunderTest.java
@@ -7,18 +7,24 @@ public class ThunderTest {
 
     @Test
     public void testOrderCalculation() {
-        // Setup
         Customer c = new Customer("Test User", "123", "Address");
         Order o = new Order(1, c);
-        
+
         MenuItem m1 = new MenuItem("Pide", 100.0, "Food");
         MenuItem m2 = new MenuItem("Coke", 25.0, "Drink");
-        
-        // Action
+
         o.addItem(m1);
         o.addItem(m2);
-        
-        // Assert: 100 + 25 = 125
+
         assertEquals(125.0, o.getTotalAmount(), 0.01);
     }
+
+    @Test
+    public void testEmptyOrderTotal() {
+        Customer c = new Customer("Test User", "123", "Address");
+        Order o = new Order(2, c);
+
+        assertEquals(0.0, o.getTotalAmount(), 0.01);
+    }
 }
+
