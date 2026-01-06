04a83dfc88fb9b80321023297a772eef337e73fe
malzb <malzb@DESKTOP-O3OUFIR.local>
2026-01-06 17:58:55 +0300

    adding CreditCardPayment comments

--- src/com/arel/oop/CreditCardPayment.java
+++ src/com/arel/oop/CreditCardPayment.java
@@ -1,19 +1,25 @@
 package com.arel.oop;
 
+// Payment method using credit card
 public class CreditCardPayment extends PaymentMethod {
+
     private String cardNumber;
     private String expiry;
 
+    // Create a credit card payment with card details
     public CreditCardPayment(double amount, String cardNumber, String expiry) {
         super(amount);
         this.cardNumber = cardNumber;
         this.expiry = expiry;
     }
 
+    // Simple validation and payment simulation
     @Override
     public boolean processPayment() {
-        if(cardNumber.length() == 16) {
-            System.out.println("Paying $" + amount + " with Card ending in " + cardNumber.substring(12));
+        if (cardNumber.length() == 16) {
+            System.out.println(
+                "Paying $" + amount + " with card ending in " + cardNumber.substring(12)
+            );
             return true;
         }
         return false;
--- src/com/arel/oop/PaymentMethod.java
+++ src/com/arel/oop/PaymentMethod.java
@@ -1,10 +1,15 @@
 package com.arel.oop;
 
+// Base class for all payment types
 public abstract class PaymentMethod {
+
     protected double amount;
 
+    // Store payment amount
     public PaymentMethod(double amount) {
         this.amount = amount;
     }
+
+    // Each payment type must implement its own logic
     public abstract boolean processPayment();
 }
